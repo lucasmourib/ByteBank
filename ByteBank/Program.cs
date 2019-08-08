2336d0394b03c8ea35482118314300153696a7fb
lucasmoura <lucas123.moura@gmail.com>
2019-08-08 19:57:58 -0300

    First Commit

---
+++ ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Funcionario carlos = new Funcionario(1);
+			carlos.Nome = "Carlos";
+			carlos.CPF = "546.879.157-20";
+			carlos.Salario = 2000.00;
+
+			Console.WriteLine(carlos.Nome);
+			Console.WriteLine(carlos.GetBonificacao());
+			Console.ReadLine();
+		}
+	}
+}
