872da3cbf5491aeb48a887a26e9c142ac20fa080
lucas moura <lucas123.moura@gmail.com>
2019-08-27 16:17:06 -0300

    Add SistemaInterno and Login
    Trabalhamos mais a fundo com herança;
    Trabalhamos mais a fundo com abstrações.
    Multiplas heranças

---
+++ ByteBank/Funcionarios/Autenticavel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+	public abstract class Autenticavel : Funcionario
+	{
+		public string Senha { get; set; }
+		public Autenticavel(double salario, string cpf) : base(salario, cpf)
+		{
+
+		}
+		public bool Autenticar(string senha)
+		{
+			return this.Senha == senha;
+		}
+	}
+}
--- ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/Funcionarios/Diretor.cs
@@ -2,7 +2,7 @@
 
 namespace ByteBank.Funcionarios
 {
-	public class Diretor : Funcionario
+	public class Diretor : Autenticavel
 	{
 		public Diretor(string cpf) : base(5000, cpf)
 		{
